cfb3530b3bead84f02cff91a9e65361ba2b73f81
Josif Trenchovski <qc22435@bristol.ac.uk>
2025-01-29 15:00:00 +0000

    triangle rotation
    Triangle rotated with cursor. One corner points towards player

--- Assets/Scripts/TriangleController.cs
+++ Assets/Scripts/TriangleController.cs
@@ -1,33 +1,38 @@
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class TriangleController : MonoBehaviour
 {
-    //private bool preview = true;
-    //private PlayerInput playerInput;
-    //private InputAction triangleActivation;
     public GameObject trianglePrefab;
     private GameObject previewTriangle;
     public GameObject previewTrianglePrefab;
     private bool isPlacing = false;
     [SerializeField] Camera cam;
+    private float angle; // angle of cursor wrt y axis unit vector
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
     }
 
     public void ActivateTriangle()
-    {
+    {   
         cam = GetComponentInChildren<Camera>();
-        Debug.Log("Preview Triangle");
+        Vector2 mousePos = Input.mousePosition;
+        // world point of the cursor
+        Vector3 worldPoint = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+        // Direction of cursor
+        Vector3 direction = worldPoint - transform.position;
+        direction.z = 0;
+        angle = Vector3.SignedAngle(Vector3.up, direction, Vector3.forward) - 180;
+        
         if (isPlacing)
         {
-            Debug.Log("Already placing a triangle");
             return;
         }
 
-        previewTriangle = Instantiate(previewTrianglePrefab, Input.mousePosition, Quaternion.identity);
+        previewTriangle = Instantiate(previewTrianglePrefab, worldPoint, Quaternion.Euler(0, 0, angle));
         isPlacing = true;
     }
 
@@ -35,28 +40,40 @@ public class TriangleController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!Input.GetKey(KeyCode.Q))
+        {
+            isPlacing=false;
+            Destroy(previewTriangle);
+        }
         if (isPlacing)
         {
             Vector2 mousePos = Input.mousePosition;
             Vector3 worldPoint = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+            Vector3 direction = worldPoint - transform.position;
+            // This wont be needed with an orthographic camera maybe
+            direction.z = 0;
+            angle = Vector3.SignedAngle(Vector3.up, direction, Vector3.forward) - 180;
             previewTriangle.transform.position = worldPoint;
+            Debug.Log("Angle" + angle);
+            Debug.DrawLine(transform.position, transform.position + new Vector3(0, 1, 0), Color.blue, 2f);
+            Debug.DrawLine(transform.position, transform.position + (worldPoint - transform.position), Color.red, 2f);
+            previewTriangle.transform.rotation = Quaternion.Euler(0, 0, angle);
 
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(1))
             {
-                PlaceTriangle();
+                PlaceTriangle(angle);
             }
         }
     }
 
-    public void PlaceTriangle()
+    public void PlaceTriangle(float angle)
     {
-        Instantiate(trianglePrefab, previewTriangle.transform.position, Quaternion.identity);
+        Instantiate(trianglePrefab, previewTriangle.transform.position, Quaternion.Euler(0, 0, angle));
         Debug.Log("Triangle Placed");
 
         Destroy(previewTriangle);
         previewTriangle = null;
 
         isPlacing = false;
-
     }
 }
