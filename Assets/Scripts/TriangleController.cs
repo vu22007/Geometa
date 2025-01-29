9e7e55533d058bdc242172e01f96baf54fcb2fa1
Josif Trenchovski <qc22435@bristol.ac.uk>
2025-01-29 15:58:23 +0000

    Small improvements

--- Assets/Scripts/TriangleController.cs
+++ Assets/Scripts/TriangleController.cs
@@ -1,7 +1,4 @@
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class TriangleController : MonoBehaviour
 {
@@ -11,26 +8,18 @@ public class TriangleController : MonoBehaviour
     private bool isPlacing = false;
     [SerializeField] Camera cam;
     private float angle; // angle of cursor wrt y axis unit vector
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-    }
 
     public void ActivateTriangle()
     {   
         cam = GetComponentInChildren<Camera>();
         Vector2 mousePos = Input.mousePosition;
-        // world point of the cursor
+        // World point of the cursor
         Vector3 worldPoint = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
         // Direction of cursor
         Vector3 direction = worldPoint - transform.position;
+        // This wont be needed with an orthographic camera maybe
         direction.z = 0;
         angle = Vector3.SignedAngle(Vector3.up, direction, Vector3.forward) - 180;
-        
-        if (isPlacing)
-        {
-            return;
-        }
 
         previewTriangle = Instantiate(previewTrianglePrefab, worldPoint, Quaternion.Euler(0, 0, angle));
         isPlacing = true;
@@ -44,36 +33,36 @@ public class TriangleController : MonoBehaviour
         {
             isPlacing=false;
             Destroy(previewTriangle);
+            return;
         }
         if (isPlacing)
         {
             Vector2 mousePos = Input.mousePosition;
             Vector3 worldPoint = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+            
             Vector3 direction = worldPoint - transform.position;
-            // This wont be needed with an orthographic camera maybe
             direction.z = 0;
             angle = Vector3.SignedAngle(Vector3.up, direction, Vector3.forward) - 180;
             previewTriangle.transform.position = worldPoint;
-            Debug.Log("Angle" + angle);
-            Debug.DrawLine(transform.position, transform.position + new Vector3(0, 1, 0), Color.blue, 2f);
-            Debug.DrawLine(transform.position, transform.position + (worldPoint - transform.position), Color.red, 2f);
+            
             previewTriangle.transform.rotation = Quaternion.Euler(0, 0, angle);
 
+            // Place triangle if right clicked while holding Q
             if (Input.GetMouseButtonDown(1))
             {
                 PlaceTriangle(angle);
+                return;
             }
         }
     }
 
     public void PlaceTriangle(float angle)
     {
+        isPlacing = false;
+        
         Instantiate(trianglePrefab, previewTriangle.transform.position, Quaternion.Euler(0, 0, angle));
-        Debug.Log("Triangle Placed");
-
+     
         Destroy(previewTriangle);
         previewTriangle = null;
-
-        isPlacing = false;
     }
 }
