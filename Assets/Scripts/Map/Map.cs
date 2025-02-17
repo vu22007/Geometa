2ae012d1300d78679b91e0b7e9dcc613292b5555
Daniel Lovell <bp22800@bristol.ac.uk>
2025-02-17 21:33:36 +0000

    Add 2D polygon colliders to scene for buildings

--- Assets/Scripts/Map/Map.cs
+++ Assets/Scripts/Map/Map.cs
@@ -1,69 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
 using UnityEngine;
-using UnityEngine.Networking;
 
-public class Map
+public class Map : MonoBehaviour
 {
-    //public static IEnumerator LoadFromBoundingBox(double south, double west, double north, double east)
-    //{
-    //    // Construct request body
-    //    string data = "data=" + UnityWebRequest.EscapeURL("" +
-    //        $"[out:json][timeout:25];" +
-    //        $"way[building]({south}, {west}, {north}, {east});" +
-    //        $"out geom;");
-
-    //    // Send API request
-    //    UnityWebRequest webRequest = UnityWebRequest.Post("https://overpass-api.de/api/interpreter", data, "application/json");
-    //    yield return webRequest.SendWebRequest();
-
-    //    // Check for unsuccessful response
-    //    if (webRequest.result != UnityWebRequest.Result.Success)
-    //    {
-    //        Debug.LogError(webRequest.error);
-    //    }
-    //    else
-    //    {
-    //        // Get JSON response
-    //        string jsonResponse = webRequest.downloadHandler.text;
-    //        Debug.Log(jsonResponse);
-    //    }
-    //}
-
-    public static async void LoadMapFromBoundingBox(double lowLat, double lowLong, double highLat, double highLong)
+    void Start()
     {
-        // Construct request body
-        // Note: We are fetching buildings (both as ways and as relations) and roads
-        string data = "data=" + UnityWebRequest.EscapeURL("" +
-            $"[out:json][timeout:25][bbox:{lowLat},{lowLong},{highLat},{highLong}];" +
-            "(" +
-            "  way[building];" +
-            "  rel[building];" +
-            "  way[highway];" +
-            ");" +
-            "out geom;");
-
-        // Send API request
-        UnityWebRequest webRequest = UnityWebRequest.Post("https://overpass-api.de/api/interpreter", data, "application/json");
-        await webRequest.SendWebRequest();
-
-        // Check for unsuccessful response
-        if (webRequest.result != UnityWebRequest.Result.Success)
-        {
-            Debug.LogError(webRequest.error);
-            return;
-        }
-
-        // Get JSON response
-        string jsonResponse = webRequest.downloadHandler.text;
-        Debug.Log(jsonResponse);
-
-        // Remove colons from required tags
-        jsonResponse = jsonResponse.Replace("building:levels", "buildingLevels");
-
-        // Parse JSON
-        MapData mapData = JsonUtility.FromJson<MapData>(jsonResponse);
-        Debug.Log(mapData.elements.Length);
+        StartCoroutine(MapLoader.LoadMapFromBoundingBox(51.453990, -2.605788, 51.456203, -2.598647));
     }
 }
