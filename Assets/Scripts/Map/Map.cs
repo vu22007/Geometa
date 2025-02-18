0208bcc373b145a31087c481520d6936a6d8cf7a
Daniel Lovell <bp22800@bristol.ac.uk>
2025-02-18 21:33:21 +0000

    Move code into separate methods and put map-related prefabs into map folder

--- Assets/Scripts/Map/Map.cs
+++ Assets/Scripts/Map/Map.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.U2D;
-using UnityEngine.UIElements;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class Map : MonoBehaviour
 {
@@ -41,16 +39,14 @@ public class Map : MonoBehaviour
 
         // Get JSON response
         string jsonResponse = webRequest.downloadHandler.text;
-        Debug.Log(jsonResponse);
 
         // Remove colons from required tags
         jsonResponse = jsonResponse.Replace("building:levels", "buildingLevels");
 
         // Parse JSON
         MapData mapData = JsonUtility.FromJson<MapData>(jsonResponse);
-        Debug.Log(mapData.elements.Length);
 
-        // Calculate horizontal shift, vertical shift and scale required to convert GPS coords into scene position
+        // Calculate horizontal shift, vertical shift and scale required to convert GPS coords into scene point
         double xShift = lowLong + (highLong - lowLong) / 2;
         double yShift = LatToY(lowLat) + (LatToY(highLat) - LatToY(lowLat)) / 2;
         double scale = 80000;
@@ -62,60 +58,19 @@ public class Map : MonoBehaviour
             if (IsBuilding(element))
             {
                 // Get scene positions for each building vertex using GPS coords
-                Vector2[] vertices = new Vector2[element.geometry.Length];
-                for (int i = 0; i < vertices.Length; i++)
-                {
-                    MapElement.Coords coords = element.geometry[i];
-                    double xPos = (coords.lon - xShift) * scale;
-                    double yPos = (LatToY(coords.lat) - yShift) * scale;
-                    vertices[i] = new Vector2((float)xPos, (float)yPos);
-                }
+                Vector2[] vertices = GetPointsFromGPSCoords(element.geometry, xShift, yShift, scale);
 
-                // Instantiate building from prefab with the map as the parent
-                GameObject building = Instantiate(buildingPrefab, new Vector3(0, 0, 0), Quaternion.identity, transform);
-
-                // Get components
-                SpriteShapeController spriteShapeController = building.GetComponent<SpriteShapeController>();
-                Spline spline = spriteShapeController.spline;
-
-                // Add building vertices to sprite shape (ignore last vertex since it is the same as the first)
-                spline.Clear();
-                for (int i = 0; i < vertices.Length - 1; i++)
-                {
-                    // Add point to sprite shape
-                    spline.InsertPointAt(i, vertices[i]);
-                    spline.SetTangentMode(i, ShapeTangentMode.Linear);
-                }
+                // Create and add building to scene
+                AddBuildingToScene(vertices);
             }
             // Deal with roads
             else if (IsRoad(element))
             {
                 // Get scene positions for each road node using GPS coords
-                Vector2[] nodes = new Vector2[element.geometry.Length];
-                for (int i = 0; i < nodes.Length; i++)
-                {
-                    MapElement.Coords coords = element.geometry[i];
-                    double xPos = (coords.lon - xShift) * scale;
-                    double yPos = (LatToY(coords.lat) - yShift) * scale;
-                    nodes[i] = new Vector2((float)xPos, (float)yPos);
-                }
+                Vector2[] nodes = GetPointsFromGPSCoords(element.geometry, xShift, yShift, scale);
 
-                // Instantiate road from prefab with the map as the parent
-                GameObject road = Instantiate(roadPrefab, new Vector3(0, 0, 0), Quaternion.identity, transform);
-
-                // Get components
-                SpriteShapeController spriteShapeController = road.GetComponent<SpriteShapeController>();
-                Spline spline = spriteShapeController.spline;
-
-                // Add road nodes to sprite shape
-                spline.Clear();
-                for (int i = 0; i < nodes.Length; i++)
-                {
-                    // Add point to sprite shape
-                    spline.InsertPointAt(i, nodes[i]);
-                    spline.SetTangentMode(i, ShapeTangentMode.Continuous);
-                    spline.SetHeight(i, 4.0f); // Thickness of road
-                }
+                // Create and add road to scene
+                AddRoadToScene(nodes);
             }
         }
     }
@@ -125,6 +80,19 @@ public class Map : MonoBehaviour
         return System.Math.Log(System.Math.Tan(
             (latitude + 90) / 360 * System.Math.PI
         )) / System.Math.PI * 180;
+    }
+
+    Vector2[] GetPointsFromGPSCoords(MapElement.Coords[] geometry, double xShift, double yShift, double scale)
+    {
+        Vector2[] points = new Vector2[geometry.Length];
+        for (int i = 0; i < points.Length; i++)
+        {
+            MapElement.Coords coords = geometry[i];
+            double xPos = (coords.lon - xShift) * scale;
+            double yPos = (LatToY(coords.lat) - yShift) * scale;
+            points[i] = new Vector2((float)xPos, (float)yPos);
+        }
+        return points;
     }
 
     bool IsBuilding(MapElement element)
@@ -142,5 +110,44 @@ public class Map : MonoBehaviour
         return element.tags.highway == "footway" ||
                element.tags.highway == "pedestrian" ||
                element.tags.highway == "steps";
+    }
+
+    void AddBuildingToScene(Vector2[] vertices)
+    {
+        // Instantiate building from prefab with the map as the parent
+        GameObject building = Instantiate(buildingPrefab, new Vector3(0, 0, 0), Quaternion.identity, transform);
+
+        // Get components
+        SpriteShapeController spriteShapeController = building.GetComponent<SpriteShapeController>();
+        Spline spline = spriteShapeController.spline;
+
+        // Add building vertices to sprite shape (ignore last vertex since it is the same as the first)
+        spline.Clear();
+        for (int i = 0; i < vertices.Length - 1; i++)
+        {
+            // Add point to sprite shape
+            spline.InsertPointAt(i, vertices[i]);
+            spline.SetTangentMode(i, ShapeTangentMode.Linear);
+        }
+    }
+
+    void AddRoadToScene(Vector2[] nodes)
+    {
+        // Instantiate road from prefab with the map as the parent
+        GameObject road = Instantiate(roadPrefab, new Vector3(0, 0, 0), Quaternion.identity, transform);
+
+        // Get components
+        SpriteShapeController spriteShapeController = road.GetComponent<SpriteShapeController>();
+        Spline spline = spriteShapeController.spline;
+
+        // Add road nodes to sprite shape
+        spline.Clear();
+        for (int i = 0; i < nodes.Length; i++)
+        {
+            // Add point to sprite shape
+            spline.InsertPointAt(i, nodes[i]);
+            spline.SetTangentMode(i, ShapeTangentMode.Continuous);
+            spline.SetHeight(i, 4.0f); // Thickness of road
+        }
     }
 }
