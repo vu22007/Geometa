6ce33ebe5e5fa63462c933c9ff12393f9662494a
Daniel Lovell <bp22800@bristol.ac.uk>
2025-03-06 11:38:21 +0000

    Create main menu GUI

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void HostGame()
+    {
+        // TODO
+        Debug.Log("Hosting game...");
+    }
+
+    public void JoinGame()
+    {
+        // TODO
+        Debug.Log("Joining game...");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
