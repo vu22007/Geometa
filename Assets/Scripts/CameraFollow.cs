88ff052db573af26a7d8e4740c11eaaf94026b26
Josif Trenchovski <qc22435@bristol.ac.uk>
2025-03-28 21:34:15 +0000

    minimap, camera fix

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,9 +7,6 @@ public class CameraFollow : MonoBehaviour
     {
         // Lock the camera's rotation
         transform.rotation = Quaternion.identity;
-        if (lockRelativePosition){
-            transform.localPosition = new Vector3(0,0,0);
-        }
     }
 }
 
