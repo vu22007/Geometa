bbf7c0b949c8da96891b92594124ecdf4b01c012
Daniel Lovell <bp22800@bristol.ac.uk>
2025-03-31 11:44:12 +0100

    Fix networking issue

--- Assets/Scripts/Menu/Lobby.cs
+++ Assets/Scripts/Menu/Lobby.cs
@@ -303,7 +303,7 @@ public class Lobby : NetworkBehaviour, IPlayerJoined, IPlayerLeft
                 Runner.SessionInfo.IsOpen = false;
                 // Load Scene that will use the 3D Generated buildings and generate 2D map in scene
                 // Switch to map scene to start the game, and the game controller will spawn player objects using the player dicts in the network manager
-                Destroy(coordinatesDataHolder);
+                //Destroy(coordinatesDataHolder);
                 Runner.LoadScene(SceneRef.FromIndex(3));
             }
         }
